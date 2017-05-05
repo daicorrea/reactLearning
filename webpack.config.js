f4c5741d6a9f0a6266120077ac38e298dea8259c
daicorrea <daiane.ocorrea@gmail.com>
2017-05-05 15:29:17 -0300

    Returning Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,8 @@
-/*const webpack = require('webpack')
+const webpack = require('webpack')
 
 //Make everything that is inside this object visible outside of this file
 module.exports = {
     entry: './ex/index.js', //This will be the entry file for the webpack, the initial one
-    output: {
-        path: __dirname + '/public',
-        filename: './bundle.js'
-    },
-    devServer: {
-        port: 8080,
-        contentBase: './public'
-    }
-}*/
-
-
-const webpack = require('webpack')
-
-module.exports = {
-    entry: './ex/index.js',
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
