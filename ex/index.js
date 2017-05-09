28057c34e77b1dc26d567a8ebe2d1d94cb0e9a0c
daiCorrea <daiane.ocorrea@gmail.com>
2017-05-09 18:03:08 -0300

    Configuring CSS

--- ex/index.js
+++ ex/index.js
@@ -1,14 +1,8 @@
-const product = {
-    name: 'Black Bic Pen',
-    price: 1.90,
-    discount: 0.05
-}
+import "react"
+import "./style.css"
 
-function clone(obj) {
-    return { ...obj} //Using Spread to "spread" the obj into a new object that is returning, using the object-spread or object-rest plugin
-}
+export default props => (
+    <h1>Oi</h1> 
+);
 
-const newProduct = clone(product);
-newProduct.name = 'Blue Bic Pen';
-
-console.log(product, newProduct);
+console.log("Funcionou");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')//Help read .css files
 
 //Make everything that is inside this object visible outside of this file
 module.exports = {
@@ -11,19 +12,27 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    plugins: [ 
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new ExtractTextPlugin('app.css')
+    ],
       module: {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2015'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        }/*, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+        }/*, {
             test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }*/]
