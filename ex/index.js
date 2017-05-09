ceb046e06ea9445c673d347f25715b18ae72d044
daiCorrea <daiane.ocorrea@gmail.com>
2017-05-09 11:51:29 -0300

    Using the Spread Operator

--- ex/index.js
+++ ex/index.js
@@ -1,5 +1,14 @@
-import Person from './person'
+const product = {
+    name: 'Black Bic Pen',
+    price: 1.90,
+    discount: 0.05
+}
 
-const person = new Person("Daiane");
-console.log(person);
+function clone(obj) {
+    return { ...obj} //Using Spread to "spread" the obj into a new object that is returning, using the object-spread or object-rest plugin
+}
 
+const newProduct = clone(product);
+newProduct.name = 'Blue Bic Pen';
+
+console.log(product, newProduct);
