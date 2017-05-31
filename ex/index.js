ecccca23ed1bdeb01d1201a14fb45239ad4939c2
daiCorrea <daiane.ocorrea@gmail.com>
2017-05-31 10:56:59 -0300

    Separating Projects

--- ex/index.js
+++
@@ -1,8 +0,0 @@
-import "react"
-import "./style.css"
-
-export default props => (
-    <h1>Oi</h1> 
-);
-
-console.log("Funcionou");
---
+++ webpackExercises/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.jsx', //This will be the entry file for the webpack, the initial one
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+      module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }]
+    }
+}
